91a515ce6e7106bd6782cc07eafbc2a29d834ee7
matteo.omicini <m.omicini@studenti.unimol.it>
2018-04-18 11:51:00 +0200

    initial native RN commit

---
+++ src/screens/Hello/Hello.js
@@ -0,0 +1,97 @@
+import React, {Component} from 'react';
+import {View, StyleSheet, StatusBar} from 'react-native';
+import {Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text} from 'native-base';
+
+export default class Hello extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            time: "",
+        };
+    }
+
+    componentDidMount() {
+        let date = new Date();
+        var hour = date.getHours();
+        this.setState({
+            time: hour
+        })
+    }
+
+    render() {
+        return (
+            <Container>
+                <StatusBar
+                    backgroundColor="#1abc9c"
+                    barStyle="light-content"
+                />
+                <View style={styles.container}>
+                    <View style={styles.box}>
+                        <Text style={{
+                                alignSelf: 'center',
+                                color: '#ecf0f1',
+                                fontSize:25
+                            }}>{this.getHello()}
+                        </Text>
+                        <Text style={styles.text}>
+                            Sir
+                        </Text>
+
+                        <Button rounded style={styles.button} onPress={
+                            () => this.props.navigation.navigate('List')
+                        }>
+                            <Text>Go</Text>
+                        </Button>
+                    </View>
+                </View>
+            </Container>
+
+        );
+    }
+
+    getHello() {
+        t = this.state.time;
+        if ((t >= "00") && (t < "06")) {
+            return "Vada a dormire";
+        }
+        if ((t >= "06") && (t < "13")) {
+            return "Good morning";
+        }
+        if ((t >= "13") && (t < "18")) {
+            return "Good afternoon";
+        }
+        if ((t >= "18") && (t < "24")) {
+            return "Buona";
+        }
+    }
+}
+
+const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#34495e',
+            alignItems: 'center',
+            justifyContent: 'center'
+        },
+        center: {
+            alignSelf: 'center'
+        },
+        text: {
+            color: '#ecf0f1',
+            fontSize: 50,
+            alignSelf: 'center'
+        },
+        button: {
+            margin: 10,
+            alignSelf: 'center',
+            backgroundColor: '#3498db'
+        },
+        box: {
+            padding: 20,
+            backgroundColor: '#1abc9c',
+            margin: 50,
+            borderRadius: 20
+        }
+    })
+    ;
