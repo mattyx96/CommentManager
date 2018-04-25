aa488b2b5b4969fe475c69d1a3ec250531bf02b4
matteo.omicini <m.omicini@studenti.unimol.it>
2018-04-25 18:40:11 +0200

    initial RN-firebase commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+/**
+ * Created by Mattyx on 03/04/18.
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Content, Container, Root} from 'native-base';
+import {StackNavigator} from 'react-navigation';
+
+
+
+import ListComment from './screens/ListComment/ListComment';
+import Hello from './screens/Hello/Hello';
+
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            fcm_token: ""
+        };
+    }
+
+    render() {
+        return (
+            <Root>
+                <RootNavigator/>
+            </Root>
+        );
+    }
+}
+
+
+const RootNavigator = StackNavigator(
+    {
+        Hello: {screen: Hello},
+        List: {screen: ListComment}
+    },
+    {
+        initialRouteName: 'Hello',
+        headerMode: "none",
+    }
+);
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: '#000000'
+    }
+});
